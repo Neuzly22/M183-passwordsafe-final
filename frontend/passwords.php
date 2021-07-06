7c0aacd38275306c11d8f60484b1ec3db71850b1
Noah <noah.leo@hispeed.ch>
2021-07-06 12:45:18 +0200

    [FEATURE]

--- frontend/passwords.php
+++ frontend/passwords.php
@@ -1,4 +1,29 @@
 <?php
+if($_POST['password-password']){
+    $unencryptedPW = $_POST['password-password'];
+    $ciphering = "AES-128-CTR";
+  
+    // Use OpenSSl Encryption method
+    $iv_length = openssl_cipher_iv_length($ciphering);
+    $options = 0;
+      
+    // Non-NULL Initialization Vector for encryption
+    $encryption_iv = '1234567891011121';
+      
+    // Store the encryption key
+    $encryption_key = "Modul183Modul183";
+      
+    // Use openssl_encrypt() function to encrypt the data
+    $encryption = openssl_encrypt($unencryptedPW, $ciphering,
+    $encryption_key, $options, $encryption_iv);
+
+    $link1 = mysqli_connect('127.0.0.1', 'root', '', 'passwordsafe');
+   
+    
+    $sqlPW = "INSERT INTO password (password_title, password_password, password_notes)
+    VALUES ('$_POST["password-title"]', '$_POST["password-password"]', '$_POST["password-notes"]');
+    $link1->query($sqlPW)
+}
 
 
 ?>
@@ -54,7 +79,7 @@ Notizen:
 <textarea name="password-notes">
 </textarea>
 </label>
-<input type="submit" value="Erstellen" onclick="">
+<input type="submit" value="Erstellen">
 </form>
 
 </body>
